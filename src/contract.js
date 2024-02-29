c730dac81dc3fe276aca8697dbced49c3f2f24f5
kirqz23 <kiryll.kuzniecow@gmail.com>
2024-02-29 23:35:08 +0100

    add new files

---
+++ src/contract.js
@@ -0,0 +1,72 @@
+import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import FlightSuretyData from '../../build/contracts/FlightSuretyData.json';
+import Config from './config.json';
+import Web3 from 'web3';
+
+export default class Contract {
+    constructor(network, callback) {
+
+        this.config = Config[network];
+        this.web3 = new Web3(new Web3.providers.HttpProvider(this.config.url));
+        this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, this.config.appAddress);
+        this.flightSuretyData = new this.web3.eth.Contract(FlightSuretyData.abi, this.config.dataAddress);
+        this.initialize(callback);
+        this.owner = null;
+        this.airlines = [];
+        this.passengers = [];
+    }
+
+    initialize(callback) {
+        this.web3.eth.getAccounts((error, accts) => {
+
+            this.owner = accts[0];
+
+            let counter = 1;
+
+            while (this.airlines.length < 5) {
+                this.airlines.push(accts[counter++]);
+            }
+
+            while (this.passengers.length < 5) {
+                this.passengers.push(accts[counter++]);
+            }
+
+            // authorize app contract to run data functions
+            this.flightSuretyData.methods.authorizeCaller(this.config.appAddress).send({ from: this.owner });
+
+            callback();
+        });
+    }
+
+    isOperational(callback) {
+        let self = this;
+        self.flightSuretyApp.methods
+            .isOperational()
+            .call({ from: self.owner }, callback);
+    }
+
+    fetchFlightStatus(flight, callback) {
+        let self = this;
+        let payload = {
+            airline: self.airlines[0],
+            flight: flight,
+            timestamp: Math.floor(Date.now() / 1000)
+        }
+        self.flightSuretyApp.methods
+            .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
+            .send({ from: self.owner }, (error, result) => {
+                callback(error, payload);
+            });
+    }
+
+    registerAirline(address, name, callback) {
+        let self = this
+        self.flightSuretyApp.methods.registerAirline(address, name).send(callback);
+    }
+
+    registerFlight(name, callback) {
+        let self = this
+        self.flightSuretyApp.methods.registerFlight(name).send(callback);
+    }
+
+}
---
+++ src/index.js
@@ -0,0 +1,73 @@
+
+import DOM from './dom';
+import Contract from './contract';
+import './flightsurety.css';
+
+
+(async () => {
+
+    let result = null;
+
+    let contract = new Contract('localhost', () => {
+
+        // Read transaction
+        contract.isOperational((error, result) => {
+            console.log(error, result);
+            display('Operational Status', 'Check if contract is operational', [{ label: 'Operational Status', error: error, value: result }]);
+        });
+
+
+        // User-submitted transaction
+        DOM.elid('submit-oracle').addEventListener('click', () => {
+            let flight = DOM.elid('flight-number').value;
+            // Write transaction
+            contract.fetchFlightStatus(flight, (error, result) => {
+                display('Oracles', 'Trigger oracles', [{ label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp }]);
+            });
+        });
+
+        // Register Airline
+        DOM.elid('register-airline').addEventListener('click', () => {
+            let name = DOM.elid('reg-airline-name').value;
+            let address = DOM.elid('reg-airline-address').value;
+
+            contract.registerAirline(address, name, (error, result) => {
+                display('Airlines', 'Register airline', [{ label: 'Airline', error: error, value: result }]);
+            });
+        });
+
+        // Register Flight
+        DOM.elid('register-flight').addEventListener('click', () => {
+            let name = DOM.elid('reg-flight-number').value;
+
+            contract.registerFlight(name, (error, result) => {
+                display('Flights', 'Register flight', [{ label: 'Flight', error: error, value: result }]);
+            });
+        });
+    });
+
+
+})();
+
+
+function display(title, description, results) {
+    let displayDiv = DOM.elid("display-wrapper");
+    let section = DOM.section();
+    section.appendChild(DOM.h2(title));
+    section.appendChild(DOM.h5(description));
+    results.map((result) => {
+        let row = section.appendChild(DOM.div({ className: 'row' }));
+        row.appendChild(DOM.div({ className: 'col-sm-4 field' }, result.label));
+        row.appendChild(DOM.div({ className: 'col-sm-8 field-value' }, result.error ? String(result.error) : String(result.value)));
+        section.appendChild(row);
+    })
+    displayDiv.append(section);
+
+}
+
+
+
+
+
+
+
